85755b59070f5283f1626c838d8633006e2658e6
quincysire <quincy.sire@student.moringaschool.com>
2024-06-28 20:02:59 +0300

    Added code for migration, flask-sqlachemy and sending data between web servers

--- server/seed.py
+++ server/seed.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
 
+from random import choice as rc
+
+from faker import Faker
+
 from app import app
-from models import db, Customer, Review, Item
+from models import db, Owner, Pet
+
+#db.init_app(app)
+
+fake = Faker()
 
 with app.app_context():
 
-    Customer.query.delete()
-    Review.query.delete()
-    Item.query.delete()
+    Pet.query.delete()
+    Owner.query.delete()
 
-    customer1 = Customer(name='Tal Yuri')
-    customer2 = Customer(name='Raha Rosario')
-    customer3 = Customer(name='Luca Mahan')
-    db.session.add_all([customer1, customer2, customer3])
-    db.session.commit()
+    owners = []
+    for n in range(50):
+        owner = Owner(name=fake.name())
+        owners.append(owner)
 
-    item1 = Item(name='Laptop Backpack', price=49.99)
-    item2 = Item(name='Insulated Coffee Mug', price=9.99)
-    item3 = Item(name='6 Foot HDMI Cable', price=12.99)
-    db.session.add_all([item1, item2, item3])
-    db.session.commit()
+    db.session.add_all(owners)
 
-    db.session.add(Review(comment="zipper broke the first week",
-                   customer=customer1, item=item1))
-    db.session.add(Review(comment="love this backpack!",
-                   customer=customer2, item=item1))
-    db.session.add(Review(comment="coffee stays hot for hours!",
-                   customer=customer1, item=item2))
-    db.session.add(Review(comment="best coffee mug ever!",
-                   customer=customer3, item=item2))
-    db.session.add(Review(comment="cable too short",
-                   customer=customer3, item=item3))
+    pets = []
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+    for n in range(100):
+        pet = Pet(name=fake.first_name(), species=rc(species), owner=rc(owners))
+        pets.append(pet)
+
+    db.session.add_all(pets)
     db.session.commit()
